87fd5c54756c5d31c0bb025bb571feb1469cec68
David Gohel <david.gohel@gmail.com>
2014-04-19 14:44:47 +0200

    src reorg and cleaning

--- R/addPlot.html.R
+++ R/addPlot.html.R
@@ -100,11 +100,13 @@ addPlot.html = function(doc, fun, pointsize=getOption("ReporteRs-fontsize"), vec
 			, ps=pointsize, fontname = fontname
 			, canvas_id = as.integer(doc$canvas_id) )
 		fun(...)
+		last_canvas_id = .C("get_current_canvas_id", (dev.cur()-1L), 0L)[[2]]
 		dev.off()
 
 		plot_ids = get("plot_ids", envir = env )
-		doc$canvas_id = get("canvas_id", envir = env )
-		
+		if( last_canvas_id < 0 ) stop("unexpected error, could not find device information.")
+		else doc$canvas_id = last_canvas_id;
+
 		jimg = .jnew( class.html4r.RaphaelList )
 		
 		for(i in 1:length( plot_ids ) ){
