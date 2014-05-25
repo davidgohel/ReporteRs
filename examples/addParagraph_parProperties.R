420feb387f9fe66d89824b6dc2aa41ce3d2f1c62
David Gohel <david.gohel@gmail.com>
2014-05-25 03:52:30 +0200

    shape pos definition

--- examples/addParagraph_parProperties.R
+++ examples/addParagraph_parProperties.R
@@ -1,4 +1,4 @@
 
 # Add my.pars into the document doc
 doc = addParagraph(doc, my.pars
-  , par.properties=parProperties(text.align="center", padding=24) )
+		, par.properties=parProperties(text.align="center", padding=24) )
