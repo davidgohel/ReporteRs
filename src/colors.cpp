ca1dd3a65124eaee1aa5342aa100ca43d0dc68bc
David Gohel <david.gohel@gmail.com>
2014-08-12 16:58:28 +0200

    header comments

--- src/colors.cpp
+++ src/colors.cpp
@@ -2,10 +2,6 @@
  * This file is part of ReporteRs.
  * Copyright (c) 2014, David Gohel All rights reserved.
  *
- * It is inspired from sources of R package grDevices:
- * Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
- * Copyright (C) 1998--2014  The R Core Team
- *
  * ReporteRs is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
