67e5f83060ff30b1f54a84c024ba14cc0847b68b
David Gohel <david.gohel@gmail.com>
2014-10-02 12:28:25 +0200

    add list style and list level in parProperties

--- R/parProperties.R
+++ R/parProperties.R
@@ -23,6 +23,9 @@
 #' @param padding.right paragraph right padding - 0 or positive integer value.
 #' @param padding paragraph padding - 0 or positive integer value. Argument \code{padding} overwrites
 #' arguments \code{padding.bottom}, \code{padding.top}, \code{padding.left}, \code{padding.right}.
+#' @param list.style list style - a single character value, expected value 
+#' is one of 'none' (default), 'unordered', 'ordered'.
+#' @param level list level if argument \code{list} is not 'none'.
 #' @return a \code{parProperties} object
 #' @export
 #' @examples
@@ -35,50 +38,61 @@
 #' , \code{\link{addPlot.docx}}
 parProperties = function(text.align = "left"
 		, padding.bottom = 1, padding.top = 1
-		, padding.left = 1, padding.right = 1, padding) {
-
-		out = list( "text.align" = "left"
-			, "padding.bottom" = 1, "padding.top" = 1
-			, "padding.left" = 1, "padding.right" = 1 )
-
-		if( !missing( padding ) ){
-			if( is.numeric( padding ) ) {
-				if( as.integer( padding ) < 0 || !is.finite( as.integer( padding ) ) ) stop("invalid padding : ", padding )
-				padding.bottom = padding.top = padding.right = padding.left = as.integer( padding )
-			} else {
-				stop("padding must be a integer value ( >=0 ).")
-			}
-		}
-		
-		if( is.character( text.align ) ){
-			match.arg( text.align, choices = c("left", "right", "center", "justify"), several.ok = F )
-			out$text.align = text.align
-		} else stop("text.align must be a character scalar ('left' | 'right' | 'center' | 'justify').")
-		
-		
-		if( is.numeric( padding.bottom ) ) {
-			if( as.integer( padding.bottom ) < 0 || !is.finite( as.integer( padding.bottom ) ) ) stop("invalid padding.bottom : ", padding.bottom)
-			out$padding.bottom = as.integer( padding.bottom )
-		} else stop("padding.bottom must be a numeric scalar (pixel unit).")
+		, padding.left = 1, padding.right = 1, padding, list.style = "none", level = 1) {
 	
-		if( is.numeric( padding.top ) ) {
-			if( as.integer( padding.top ) < 0 || !is.finite( as.integer( padding.top ) ) ) stop("invalid padding.top : ", padding.top)
-			out$padding.top = as.integer( padding.top )
-		} else stop("padding.top must be a numeric scalar (pixel unit).")
-		
-		if( is.numeric( padding.left ) ) {
-			if( as.integer( padding.left ) < 0 || !is.finite( as.integer( padding.left ) ) ) stop("invalid padding.left : ", padding.left)
-			out$padding.left = as.integer( padding.left )
-		} else stop("padding.left must be a numeric scalar (pixel unit).")
-		
-		if( is.numeric( padding.right ) ) {
-			if( as.integer( padding.right ) < 0 || !is.finite( as.integer( padding.right ) ) ) stop("invalid padding.right : ", padding.right)
-			out$padding.right = as.integer( padding.right )
-		} else stop("padding.right must be a numeric scalar (pixel unit).")
-		
-		class( out ) = "parProperties"
-		
-		out
+	out = list( "text.align" = "left"
+			, "padding.bottom" = 1, "padding.top" = 1
+			, "padding.left" = 1, "padding.right" = 1, list = "none", level = 1)
+	
+	if( !missing( padding ) ){
+		if( is.numeric( padding ) ) {
+			if( as.integer( padding ) < 0 || !is.finite( as.integer( padding ) ) ) stop("invalid padding : ", padding )
+			padding.bottom = padding.top = padding.right = padding.left = as.integer( padding )
+		} else {
+			stop("padding must be a integer value ( >=0 ).")
+		}
+	}
+	
+	if( is.character( text.align ) ){
+		match.arg( text.align, choices = c("left", "right", "center", "justify"), several.ok = F )
+		out$text.align = text.align
+	} else stop("text.align must be a character scalar ('left' | 'right' | 'center' | 'justify').")
+	
+	
+	if( is.numeric( padding.bottom ) ) {
+		if( as.integer( padding.bottom ) < 0 || !is.finite( as.integer( padding.bottom ) ) ) stop("invalid padding.bottom : ", padding.bottom)
+		out$padding.bottom = as.integer( padding.bottom )
+	} else stop("padding.bottom must be a numeric scalar (pixel unit).")
+	
+	if( is.numeric( padding.top ) ) {
+		if( as.integer( padding.top ) < 0 || !is.finite( as.integer( padding.top ) ) ) stop("invalid padding.top : ", padding.top)
+		out$padding.top = as.integer( padding.top )
+	} else stop("padding.top must be a numeric scalar (pixel unit).")
+	
+	if( is.numeric( padding.left ) ) {
+		if( as.integer( padding.left ) < 0 || !is.finite( as.integer( padding.left ) ) ) stop("invalid padding.left : ", padding.left)
+		out$padding.left = as.integer( padding.left )
+	} else stop("padding.left must be a numeric scalar (pixel unit).")
+	
+	if( is.numeric( padding.right ) ) {
+		if( as.integer( padding.right ) < 0 || !is.finite( as.integer( padding.right ) ) ) stop("invalid padding.right : ", padding.right)
+		out$padding.right = as.integer( padding.right )
+	} else stop("padding.right must be a numeric scalar (pixel unit).")
+	
+	if( is.character( list.style ) ) {
+		match.arg( list.style, choices = c('none', 'ordered', 'unordered'), several.ok = F )
+		out$list.style = list.style
+	} else stop("list.style must be a character value ('none' | 'ordered' | 'unordered').")
+	
+	if( is.numeric( level ) ) {
+		if( as.integer( level ) < 0 || !is.finite( as.integer( level ) ) ) stop("invalid level value: ", level)
+		out$level = as.integer( level )
+	} else stop("level must be a positive integer value (1 to 9).")
+	
+	
+	class( out ) = "parProperties"
+	
+	out
 }
 
 #' @title Modify paragraph formatting properties 
@@ -99,7 +113,7 @@ parProperties = function(text.align = "left"
 #' @S3method chprop parProperties
 chprop.parProperties <- function(object, text.align
 		, padding.bottom, padding.top
-		, padding.left, padding.right, padding, ...) {
+		, padding.left, padding.right, padding, list.style, level, ...) {
 	
 	if( !missing( padding ) ){
 		if( is.numeric( padding ) ) {
@@ -146,6 +160,22 @@ chprop.parProperties <- function(object, text.align
 		} else stop("padding.right must be a numeric scalar (pixel unit).")
 	}
 	
+	
+	
+	if( !missing( list.style ) ){
+		if( is.character( list.style ) ) {
+			match.arg( list.style, choices = c('none', 'ordered', 'unordered'), several.ok = F )
+			object$list.style = list.style
+		} else stop("list.style must be a character value ('none' | 'ordered' | 'unordered').")
+	}
+	if( !missing( level ) ){
+		if( is.numeric( level ) ) {
+			if( as.integer( level ) < 0 || !is.finite( as.integer( level ) ) ) stop("invalid level value: ", level)
+			object$level = as.integer( level )
+		} else stop("level must be a positive integer value (1 to 9).")
+		
+	}
+	
 	object					
 }
 
@@ -157,7 +187,9 @@ print.parProperties = function (x, ...){
 	cat( "padding-bottom:" , x$padding.bottom, ";" )
 	cat( "padding-top:", x$padding.top, ";" )
 	cat( "padding-left:", x$padding.left, ";" )
-	cat( "padding.right:", x$padding.right, ";}\n" )
+	cat( "padding.right:", x$padding.right, ";" )
+	cat( "list.style:", x$list.style, ";" )
+	cat( "level:", x$level, ";}\n" )
 }
 
 
@@ -165,7 +197,8 @@ print.parProperties = function (x, ...){
 	jparProp = .jnew(class.text.ParProperties
 			, robject$text.align
 			, robject$padding.bottom, robject$padding.top
-			, robject$padding.left, robject$padding.right )
-
+			, robject$padding.left, robject$padding.right, 
+			robject$list.style, robject$level )
+	
 	jparProp
 }
