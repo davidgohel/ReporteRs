e01d180700c40a45a06059151baaacb8059bfd01
David Gohel <david.gohel@gmail.com>
2014-10-20 21:06:15 +0200

    add shading color for textProperties objects

--- examples/pot1_example.R
+++ examples/pot1_example.R
@@ -1,4 +1,4 @@
 
 # "My tailor is rich" with formatting on some words
 pot1 = pot("My tailor", textProperties(color = "red" ) 
-  ) + " is " + pot("rich", textProperties( font.weight = "bold" ) )
+  ) + " is " + pot("rich", textProperties(shading.color = "red", font.weight = "bold" ) )
