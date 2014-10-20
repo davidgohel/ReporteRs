7b426cf8396ba8d5d9fb42f47d234523c5835bf6
David Gohel <david.gohel@gmail.com>
2014-10-21 01:02:00 +0200

    doc

--- R/ReporteRs.R
+++ R/ReporteRs.R
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #' Package: \tab ReporteRs\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.6.5\cr
-#' Date: \tab 2014-10-16\cr
+#' Version: \tab 0.6.6\cr
+#' Date: \tab 2014-10-21\cr
 #' License: \tab GPL (>= 3)\cr
 #' LazyLoad: \tab yes\cr
 #' }
