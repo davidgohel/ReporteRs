f4676c3ab81a83544dce9d190ceb0f51e71c2a9f
David Gohel <david.gohel@gmail.com>
2014-11-17 23:00:02 +0100

    clean things

--- examples/BoostrapMenu.R
+++
@@ -1,9 +0,0 @@
-mymenu = BoostrapMenu( title = "my title")
-
-mydd = DropDownMenu( label = "Mon menu" )
-mydd = addLinkItem( mydd, label = "GitHub", "http://github.com/")
-mydd = addLinkItem( mydd, separator.after = TRUE)
-mydd = addLinkItem( mydd, label = "Wikipedia", "http://www.wikipedia.fr")
-
-mymenu = addLinkItem( mymenu, label = "ReporteRs", "http://github.com/davidgohel/ReporteRs")
-mymenu = addLinkItem( mymenu, dd = mydd )
