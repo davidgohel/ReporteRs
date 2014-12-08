749434c70515f116be6d7bd2736d869f67f180d0
David Gohel <david.gohel@gmail.com>
2014-12-08 17:01:10 +0100

    delete pbc_summary

--- examples/FlexTable.pbc.header.R
+++
@@ -1,3 +0,0 @@
-data(pbc_summary)
-
-MyFTable = FlexTable( data = pbc_summary[, 1:4] )
--- examples/FlexTable.pbc.no.header.R
+++
@@ -1,5 +0,0 @@
-data(pbc_summary)
-
-MyFTable = FlexTable( data = pbc_summary[, 1:4]
-  , header.columns = FALSE
-)
