17ccf2605415c328499e454720e0f924774e1c72
David Gohel <david.gohel@gmail.com>
2015-01-21 22:08:32 +0100

    vanilla.table improvment

--- R/FlexTable_theme.R
+++ R/FlexTable_theme.R
@@ -6,24 +6,25 @@
 #' @param dataset the data to use
 #' @param double.format format string for \code{double} column to 
 #' format in the dataset. See argument \code{fmt} of \code{\link{sprintf}}.
+#' @param add.rownames logical value - should the row.names be included in the table. 
 #' @export
 #' @examples
 #' #START_TAG_TEST
 #' vanilla.table( iris)
 #' @example examples/STOP_TAG_TEST.R
 #' @seealso \code{\link{FlexTable}}
-vanilla.table = function( dataset, double.format = "%0.3f" ){
+vanilla.table = function( dataset, double.format = "%0.3f", add.rownames = FALSE ){
 	for(j in names( dataset ) ){
 		if( is.numeric( dataset[, j] ) )
 			dataset[, j] = sprintf(double.format, dataset[, j] )
 	}
 	
-	ft = FlexTable( dataset, add.rownames = TRUE )
+	ft = FlexTable( dataset, add.rownames = add.rownames )
 	
 	ft[,,to="header"] = textBold()
-	ft[,,to="header"] = parRight()
+	ft[,,to="header"] = parRight(padding.left = 4, padding.right = 4)
 	ft[,,to="body"] = textNormal()
-	ft[,,to="body"] = parRight()
+	ft[,,to="body"] = parRight(padding.left = 4, padding.right = 4)
 	
 	ft = setFlexTableBorders( ft
 			, inner.vertical = borderProperties( width=0 )
