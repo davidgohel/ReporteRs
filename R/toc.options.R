f3873d97e4f8bc75c7b43dd55712da108ff82b6b
David Gohel <david.gohel@ardata.fr>
2015-08-03 18:32:50 +0200

    drop url

--- R/toc.options.R
+++ R/toc.options.R
@@ -11,7 +11,8 @@
 #' occuring when list separator used when building the TOC is 
 #' different from the list separator in your computer's regional settings.
 #' 
-#' see \url{http://support.microsoft.com/kb/302865/EN-US}
+#' See entry 302865 of Microsoft knowledge database for more information.
+#' 
 #' @examples
 #' #START_TAG_TEST
 #' doc = docx( title = "title" )
