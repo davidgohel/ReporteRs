89e108da6797207e6e364855ed207000b99efe84
David Gohel <david.gohel@ardata.fr>
2016-01-12 16:25:42 +0100

    drop size calculation

--- R/addImage.docx.R
+++ R/addImage.docx.R
@@ -1,21 +1,20 @@
 #' @title Add external image into a docx object
 #'
 #' @description Add external images into a \code{\link{docx}} object.
-#' 
+#'
 #' @param doc Object of class \code{\link{docx}} where external image has to be added
 #' @param filename \code{"character"} value, complete filename of the external image
-#' @param bookmark a character value ; id of the Word bookmark to replace by the image. 
+#' @param bookmark a character value ; id of the Word bookmark to replace by the image.
 #' optional. if missing, image is added at the end of the document. See \code{\link{bookmark}}.
-#' @param par.properties paragraph formatting properties of the paragraph that contains images. 
+#' @param par.properties paragraph formatting properties of the paragraph that contains images.
 #' An object of class \code{\link{parProperties}}
 #' @param width image width in inches
 #' @param height image height in inches
-#' @param ppi dot per inches, default to 72
-#' @param ... further arguments, not used. 
+#' @param ... further arguments, not used.
 #' @return an object of class \code{\link{docx}}.
-#' @details 
-#' If arguments width and height are missing, values will be defined as 
-#' their respective number of pixels divide by \code{ppi}.
+#' @details
+#' Arguments \code{width} and \code{height} can be defined with functions
+#' \code{png::readPNG}, \code{jpeg::readJPEG} or \code{bmp::read.bmp}.
 #' @examples
 #' #START_TAG_TEST
 #' doc.filename = "addImage_example.docx"
@@ -28,23 +27,27 @@
 #' , \code{\link{addImage}}, \code{\link{bookmark}}
 #' @export
 addImage.docx = function(doc, filename, bookmark,
-	par.properties = parProperties(text.align = "center", padding = 5 ), 
-	width, height, ppi = 72, ... ) {
+	par.properties = parProperties(text.align = "center", padding = 5 ),
+	width, height, ... ) {
 
-	if( grepl("\\.(wmf|emf)$", filename ) ){
-		if( missing( width ) || missing(height) )
-			stop("when using wmf or emf file, you must specify argument width and height.")
-	}
+  if( missing(width) )
+    stop("width can not be missing")
+  if( missing(height) )
+    stop("height can not be missing")
 
-	jimg = .jnew(class.Image , filename, as.integer(ppi) )
-	if( !missing( width ) && !missing(height) )
-		.jcall( jimg, "V", "setDim", as.double( width ), as.double( height ) )
-	
+
+  if( !is.numeric( width ) )
+    stop("arguments width must be a numeric vector")
+  if( !is.numeric( height ) )
+    stop("arguments height must be a numeric vector")
+
+  jimg = .jnew(class.Image , filename, .jfloat( width ), .jfloat( height ) )
+
 	if( missing( bookmark ) )
 		.jcall( doc$obj, "V", "add", jimg
 				, .jParProperties( par.properties ) )
-	else .jcall( doc$obj, "V", "add", jimg, 
+	else .jcall( doc$obj, "V", "add", jimg,
 				.jParProperties( par.properties ), bookmark )
-	
+
 	doc
 }
--- R/addImage.pptx.R
+++ R/addImage.pptx.R
@@ -1,26 +1,25 @@
 #' @title Insert an external image into a pptx object
 #'
 #' @description Add an external image into a \code{\link{pptx}} object.
-#' 
+#'
 #' @param doc \code{\link{pptx}} object where external image has to be added
 #' @param filename \code{"character"} value, complete filename of the external image
+#' @param width width of the shape in inches. See details.
+#' @param height height of the shape in inches. See details.
 #' @param offx optional, x position of the shape (top left position of the bounding box) in inches. See details.
 #' @param offy optional, y position of the shape (top left position of the bounding box) in inches See details.
-#' @param width optional, width of the shape in inches See details.
-#' @param height optional, height of the shape in inches See details.
-#' @param ppi dot per inches, default to 72
-#' @param ... further arguments, not used. 
-#' @details 
-#' By default, image is added to the next free 'content' shape of the current slide. 
+#' @param ... further arguments, not used.
+#' @details
+#' By default, image is added to the next free 'content' shape of the current slide.
 #' See \code{\link{slide.layouts.pptx}} to view the slide layout.
-#' 
-#' If arguments width and height are missing, values will be defined as 
-#' their respective number of pixels divide by \code{ppi}.
-#' 
-#' If arguments offx and offy are missing, position is defined as 
-#' the position of the next available shape of the slide. This 
-#' dimensions can be defined in the layout of the PowerPoint template used to create 
-#' the \code{pptx} object. 
+#'
+#' Arguments \code{width} and \code{height} can be defined with functions
+#' \code{png::readPNG}, \code{jpeg::readJPEG} or \code{bmp::read.bmp}.
+#'
+#' If arguments offx and offy are missing, position is defined as
+#' the position of the next available shape of the slide. This
+#' dimensions can be defined in the layout of the PowerPoint template used to create
+#' the \code{pptx} object.
 #' @return an object of class \code{\link{pptx}}.
 #' @examples
 #' #START_TAG_TEST
@@ -30,36 +29,35 @@
 #' @example examples/addImageWMFPresentation.R
 #' @example examples/writeDoc_file.R
 #' @example examples/STOP_TAG_TEST.R
-#' @seealso \code{\link{pptx}}, \code{\link{addPlot.pptx}}
-#' , \code{\link{addImage}}
+#' @seealso \code{\link{pptx}}, \code{\link{addPlot.pptx}},
+#' \code{\link{addImage}}
 #' @export
-addImage.pptx = function(doc, filename, offx, offy, width, height, ppi = 72, ... ) {
-	
-	slide = doc$current_slide 
-	check.dims = sum( c( !missing( offx ), !missing( offy ), !missing( width ), !missing( height ) ) )
-	
-	if( !missing(offx) && !is.numeric( offx ) ) stop("arguments offx must be a numeric vector")
-	if( !missing(offy) && !is.numeric( offy ) ) stop("arguments offy must be a numeric vector")
-	if( !missing(width) && !is.numeric( width ) ) stop("arguments width must be a numeric vector")
-	if( !missing(height) && !is.numeric( height ) ) stop("arguments height must be a numeric vector")
+addImage.pptx = function(doc, filename, offx, offy, width, height, ... ) {
 
-	jimg = .jnew(class.Image , filename, as.integer(ppi) )
-	if( !missing( width ) && !missing(height) )
-		.jcall( jimg, "V", "setDim", as.double( width ), as.double( height ) )
-	
-	if( check.dims > 3 ){
-		out = .jcall( slide, "I", "add", jimg
-				, as.double( offx ), as.double( offy ), as.double( width ), as.double( height ) )
-	} else if( !missing(offx) && !missing(offy) && missing(width) && missing(height) ){
-		out = .jcall( slide, "I", "add", jimg
-				, as.double( offx ), as.double( offy ) )
-	}  else if( check.dims < 1 ){
-		out = .jcall( slide, "I", "add", jimg )
+	slide = doc$current_slide
+
+	if( missing(width) )
+	  stop("width can not be missing")
+	if( missing(height) )
+	  stop("height can not be missing")
+
+	if( !missing(offx) && !is.numeric( offx ) )
+	  stop("arguments offx must be a numeric vector")
+	if( !missing(offy) && !is.numeric( offy ) )
+	  stop("arguments offy must be a numeric vector")
+	if( !is.numeric( width ) )
+	  stop("arguments width must be a numeric vector")
+	if( !is.numeric( height ) )
+	  stop("arguments height must be a numeric vector")
+
+	jimg = .jnew(class.Image , filename, .jfloat( width ), .jfloat( height ) )
+
+	if( !missing( offx ) && !missing( offy ) ){
+		out = .jcall( slide, "I", "add", jimg, .jfloat( offx ), .jfloat( offy ) )
 	} else {
-#		warning("arguments width and height ignored.")
-		out = .jcall( slide, "I", "add", jimg )
+	  out = .jcall( slide, "I", "add", jimg )
 	}
-	
+
 	if( isSlideError( out ) ){
 		stop( getSlideErrorString( out , "image") )
 	}
