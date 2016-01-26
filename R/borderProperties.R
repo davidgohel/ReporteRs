51887f5f96951fd37d2a3fb712107800fbf5c220
David Gohel <david.gohel@ardata.fr>
2016-01-26 17:16:54 +0100

    borderProperties doc refactoring

--- R/borderProperties.R
+++ R/borderProperties.R
@@ -1,44 +1,43 @@
 #' @title border properties object
 #'
 #' @description create a border properties object.
-#' 
+#'
 #' @param color border color - single character value (e.g. "#000000" or "black")
 #' @param style border style - single character value : "none" or "solid" or "dotted" or "dashed"
 #' @param width border width - an integer value : 0>= value
-#' @examples 
+#' @examples
 #' borderProperties()
 #' borderProperties(color="orange", style="solid", width=1)
 #' borderProperties(color="gray", style="dotted", width=1)
-#' @seealso \code{\link{chprop.borderProperties}}, \code{\link{alterFlexTable}}, 
-#' \code{\link{setFlexTableBorders}}
-#' @export 
+#' @seealso \code{\link{alterFlexTable}}, \code{\link{setFlexTableBorders}}
+#' @export
 borderProperties = function( color = "black", style = "solid", width = 1 ){
-	
+
 	if( length( color ) != 1 ) stop("color must be a single character value")
 	if( length( style ) != 1 ) stop("style must be a single character value")
 	if( length( width ) != 1 ) stop("width must be a single integer value")
-	
+
 	if( is.numeric( width ) ) {
-		if( as.integer( width ) < 0 || !is.finite( as.integer( width ) ) ) 
+		if( as.integer( width ) < 0 || !is.finite( as.integer( width ) ) )
 			stop("invalid width : ", width )
 	} else {
 		stop("width must be a single integer value >= 0")
 	}
-	
+
 	if( !is.character( style ) ) {
 		stop("style must be a character value.")
 	}
 	if( !is.element( style, ReporteRs.border.styles ) )
 		stop("style must be a character value (", paste( ReporteRs.border.styles, collapse = "|") ,").")
-	
+
 	if( !is.character( color ) ) {
 		stop("color must be a character value.")
 	} else if( !is.color(color) ){
 		stop("color must be a valid color.")
 	}
-	
-	
-	out = list( 
+
+
+	out = list(
 			color = color
 			, style = style
 			, width = width
@@ -48,21 +47,21 @@ borderProperties = function( color = "black", style = "solid", width = 1 ){
 }
 
 
-#' @title Modify border formatting properties 
-#'
-#' @description Modify an object of class \code{\link{borderProperties}}.  
-#' @param object \code{\link{borderProperties}} object to modify
-#' @inheritParams borderProperties
-#' @param ... further arguments - not used 
-#' @return a \code{\link{borderProperties}} object
+#' @param ... further arguments - not used
+#' @param object \code{borderProperties} object to update
+#' @details
+#' Get a modified version of a \code{borderProperties} with
+#' \code{chprop}.
 #' @examples
+#'
+#' # update borderProperties --------
 #' x = borderProperties()
 #' chprop(x, color="orange", style="dashed", width=1)
 #' chprop(x, width=5)
-#' @seealso \code{\link{borderProperties}}
+#' @rdname borderProperties
 #' @export
 chprop.borderProperties <- function(object, color, style, width, ... ) {
-	
+
 	if( !missing( color ) ){
 		if( length( color ) != 1 ) stop("color must be a single character value")
 		if( !is.character( color ) ) {
@@ -79,25 +78,27 @@ chprop.borderProperties <- function(object, color, style, width, ... ) {
 		}
 		if( !is.element( style, ReporteRs.border.styles ) )
 			stop("style must be a character value (", paste( ReporteRs.border.styles, collapse = "|") ,").")
-		
+
 		object$style = style
 	}
 	if( !missing( width ) ){
 		if( length( width ) != 1 ) stop("width must be a single integer value")
-		
+
 		if( is.numeric( width ) ) {
-			if( as.integer( width ) < 0 || !is.finite( as.integer( width ) ) ) 
+			if( as.integer( width ) < 0 || !is.finite( as.integer( width ) ) )
 				stop("invalid width : ", width )
 		} else {
 			stop("width must be a single integer value >= 0")
 		}
 		object$width = width
 	}
-	
-	object					
+
+	object
 }
 
 
+#' @param x \code{borderProperties} object to print
+#' @rdname borderProperties
 #' @export
 print.borderProperties = function (x, ...){
 	cat( "borderProperties{color:", x$color, ";" )
@@ -105,16 +106,18 @@ print.borderProperties = function (x, ...){
 	cat( "width:", x$width, ";" )
 	cat( "}" )
 }
+
+#' @rdname borderProperties
 #' @export
 as.character.borderProperties = function (x, ...){
 	paste( "borderProperties{color:", x$color, ";",
 			"style:", x$style, ";", "width:", x$width, ";}", sep = "" )
-	
+
 }
 
 .jborderProperties = function( object ){
-	.jnew(class.tables.BorderProperties, 
-			as.character(object$color), 
-			as.character(object$style), as.integer( object$width ) )	
+	.jnew(class.tables.BorderProperties,
+			as.character(object$color),
+			as.character(object$style), as.integer( object$width ) )
 }
 
