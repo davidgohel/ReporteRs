92c7a2accf4662a1b0147f43e30a91fa87f1cbeb
dgohel <david.gohel@ardata.fr>
2016-11-04 17:39:30 +0100

    delete bsdoc examples

--- examples/DropDownMenu.R
+++
@@ -1,6 +0,0 @@
-mydd = DropDownMenu( label = "My menu" )
-mydd = addLinkItem( mydd, 
-	label = "GitHub", "http://github.com/", active = TRUE)
-mydd = addLinkItem( mydd, separator.after = TRUE)
-mydd = addLinkItem( mydd, 
-	label = "Wikipedia", "http://www.wikipedia.fr")
--- examples/addFooter.bsdoc.R
+++
@@ -1,7 +0,0 @@
-doc = addFooter( doc, value = pot( "Code licensed under ", 
-    format = textProperties(color="gray") ) + 
-  pot("GPL-3", format = textProperties(color="#428bca"), 
-    hyperlink = "https://gnu.org/licenses/gpl.html" ) + 
-  pot(".", format = textProperties(color="gray") ), 
-par.properties = parCenter( padding = 2 ) 
-)
