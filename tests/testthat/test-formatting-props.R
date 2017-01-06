09525f2b537ad69ac944eaec3466e52748967c3d
dgohel <david.gohel@ardata.fr>
2017-01-06 16:20:47 +0100

    testthat

---
+++ tests/testthat/test-formatting-props.R
@@ -0,0 +1,18 @@
+context("formatting properties")
+
+test_that("text properties", {
+  tp <- textProperties(color="red", font.weight = "bold",
+                       font.style = "italic", underlined = TRUE )
+  tp_ <- chprop(tp, font.weight = "normal")
+  expect_equal(tp_$font.weight, "normal" )
+})
+
+test_that("par properties", {
+  pp <- parRight( )
+  expect_equal(pp$text.align, "right" )
+  pp <- parLeft( )
+  expect_equal(pp$text.align, "left" )
+  pp_ <- chprop(pp, text.align = "center")
+  expect_equal(pp_$text.align, "center" )
+})
+
